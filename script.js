36f1a99e2ab1dba05d1189e105e36fb96dd23262
Mario Jose Gavilanez <mj@MacBook-Air-de-Mario.local>
2026-07-19 16:09:49 -0500

    Versión 2.0 - Audio y ambiente dinámico, mayor volumen un poco mas, y un poco

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ const soundButton = document.getElementById("soundToggle");
 windAudio.loop = true;
 birdsAudio.loop = true;
 
-windAudio.volume = 0.70;
-birdsAudio.volume = 0.85;
+windAudio.volume = 0.90;
+birdsAudio.volume = 0.99;
 
 let ambienceStarted = false;
 let ambienceMuted = false;
